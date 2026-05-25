c2958e15418cd36dcd068633abadb6d79b09bd31
Zoran Šaronja <zoran.saronja@gmail.com>
2026-05-25 10:35:12 +0200

    initial upload

---
+++ src/main/java/hr/pacman/mop/MopApplication.java
@@ -0,0 +1,13 @@
+package hr.pacman.mop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MopApplication.class, args);
+	}
+
+}
